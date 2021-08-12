a6ffab249b121c5eaaf49a554799e97322136151
ahmed aly <>
2021-08-11 21:32:47 -0500

    chore(formatter): run formatter

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
- uint public taskCount = 0;
+    uint public taskCount = 0;
 }
 
--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,15 @@
 module.export = {
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*"
+        }
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
     }
-  },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
-    }
-  }
 }
-
